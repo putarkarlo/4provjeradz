785a5c0f8dbf86312ad404fa8dc67e5b0914926d
putarkarlo <64776074+putarkarlo@users.noreply.github.com>
2020-05-04 17:55:16 +0200

    Update main.cpp
    Unos novog računa

--- main.cpp
+++ main.cpp
@@ -1,9 +1,63 @@
 #include <iostream>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<utility>
+#include<numeric>
 
 using namespace std;
 
 int main()
 {
     cout << "Hello World!" << endl;
+    int brKlijenata=0;
+    unsigned long long int *brRacuna=new unsigned long long int[1000];
+    unsigned long long int *brRacuna2=new unsigned long long int[1000];
+    string *prezimeIme=new string[1000];
+    string *prezimeIme2=new string[1000];
+    double *saldo=new double[1000];
+    double *saldo2=new double[1000];
+    int izbor;
+    while(1)
+    {
+
+      system("cls");
+
+      cout<<"1. Unos novog racuna"<<endl;
+      cout<<"2. ispisi sve podatke"<<endl;
+      cout<<"3. pretraga prema prezimenu i imenu"<<endl;
+      cout<<"4. brisanje racuna"<<endl;
+      cout<<"5. ispisi sortirano po prezimenu i imenu"<<endl;
+      cout<<"6. izmjena podataka prema broju racuna"<<endl;
+      cout<<"7. izlaz"<<endl;
+        cout<<"Unesite svoj odabir: ";
+        cin >> izbor;
+        if(izbor==1)
+        {
+            unsigned long long unos;
+            while(1)
+            {
+                try
+                {
+                    cout << "Unesite broj racuna: ";
+                    cin >> unos;
+                    if(unos<1000000000 || unos>9999999999)
+                        throw "Broj racuna mora imati 10 znamenki.";
+                    break;
+                }
+                catch(const char* msg)
+                {
+                    cout << msg << endl;
+                }
+            }
+            brRacuna[brKlijenata]=unos;
+            cout << "Unesite prezime i ime: " << endl;
+            cin.ignore();
+            getline(cin, prezimeIme[brKlijenata]);
+            cout << "Unesite saldo: ";
+            cin >> saldo[brKlijenata];
+            brKlijenata++;
+        }
+    }
     return 0;
 }
