468e98740ad562d9ee3c7cc50e8d4860e70d4efd
putarkarlo <64776074+putarkarlo@users.noreply.github.com>
2020-05-04 18:04:30 +0200

    Update main.cpp
    sada ispisuje račune

--- main.cpp
+++ main.cpp
@@ -7,9 +7,25 @@
 
 using namespace std;
 
+bool negativni_saldo(double saldo)
+{
+    if(saldo<0)
+        return true;
+    else
+        return false;
+
+}
+bool tekuci(unsigned long long broj)
+{
+    if(broj/10000000==32)
+        return true;
+    else
+        return false;
+
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
     int brKlijenata=0;
     unsigned long long int *brRacuna=new unsigned long long int[1000];
     unsigned long long int *brRacuna2=new unsigned long long int[1000];
@@ -20,9 +36,7 @@ int main()
     int izbor;
     while(1)
     {
-
       system("cls");
-
       cout<<"1. Unos novog racuna"<<endl;
       cout<<"2. ispisi sve podatke"<<endl;
       cout<<"3. pretraga prema prezimenu i imenu"<<endl;
@@ -58,6 +72,18 @@ int main()
             cin >> saldo[brKlijenata];
             brKlijenata++;
         }
+        else if(izbor==2)
+               {
+                   for(int i=0; i<brKlijenata; i++)
+                   {
+                       cout << brRacuna[i] << ", " << prezimeIme[i] << ", " << saldo[i] << endl;
+                   }
+                   cout<<"Suma svih salda iznosi:"<<accumulate(saldo,saldo+brKlijenata, 0.0)<<endl;
+                   int max_index=max_element(saldo,saldo+brKlijenata)-saldo;
+                   cout<<"Klijent koji ima najveci saldo: "<< prezimeIme[max_index]<<endl;
+                   cout<<"Broj racuna sa negativnim saldom: "<<count_if(saldo,saldo+brKlijenata,negativni_saldo)<<endl;
+                   cout<<"Broj tekucih racuna: "<<count_if(brRacuna,brRacuna+brKlijenata,tekuci)<<endl;
+               }
     }
     return 0;
 }
